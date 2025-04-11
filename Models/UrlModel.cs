40390407482c4fd7cf68f2ca94b909c80511f1dd
saraiya-rishi <saraiyarishi8102@gmail.com>
2025-04-11 21:44:59 +0530

    rs

--- Models/UrlModel.cs
+++ Models/UrlModel.cs
@@ -2,11 +2,13 @@
 {
     public class UrlModel
     {
+        public int Id { get; set; }
         public string LongUrl { get; set; }
         public string ShortCode { get; set; }
         public string CustomAlias { get; set; }
-        public string Password { get; set; }
         public DateTime? ExpiryDate { get; set; }
+        public string Password { get; set; }
+        public int ClickCount { get; set; }
 
     }
 }
